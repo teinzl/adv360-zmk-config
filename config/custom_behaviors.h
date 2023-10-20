f9c85c1a0c362b068451c52ed2ca58b4d8cfa1ce
Nick deLannoy <nick.delannoy@gmail.com>
2023-10-20 17:20:25 -0500

    changed a ton of things
    switched control and command home row mods, I use control pretty often with vim and command less, so gonna give my pinkies a rest. the command on the pinky also works better for some one handed shortcuts, like hitting command + number to go to a browser tab with the right hand.
    num layer is more normal again, and moved to the right hand, with the left hand a near dup of the sym layer, with added comma and dot taking the place of amps and pipe, which aren't used often when typing out numerical expressions
    sym layer positioning tweaked a bit on the left hand to make bigrams easier, added autopair macros to the right hand. right hand is set up to overwrite less from the base layer and to play with the num layer better where numpad doesn't overlap
    got rid of the question mark to bang morph, I use the slash often enough that I want it on the base layer, and the bang is still easy to get to on the sym layer
    fn layer also moved to match the num layer
    added clipboard and undo/redo commands to the nav and mouse layers
    got rid of the hold functionality on arrow keys in the nav layer
    changed all left hand home keys in the nav layer to sticky mods, changed mouse layer to plain mods as well
    quick layer is gone, with some of its functionality moved into the left hand of the mouse layer
    mod/bt and media layers have been combined into one layer, with hold keys for bluetooth profile selection on the bottom row of left hand

--- config/custom_behaviors.h
+++ config/custom_behaviors.h
@@ -3,7 +3,7 @@
 
 /* Global settings */
 
-#define QUICK_TAP_MS 150
+#define QUICK_TAP_MS 175
 
 &sk {  // sticky-key config
     release-after-ms = <600>;  // release after 0.6s
@@ -20,44 +20,9 @@
     quick-tap-ms = <QUICK_TAP_MS>;
 };
 
-/* Apple Globe key */
-// TODO - this doesn't work
-#define C_KEYBOARD_LAYOUT_SELECT (ZMK_HID_USAGE(HID_USAGE_CONSUMER, 0x029D))
-#define GLOBE C_KEYBOARD_LAYOUT_SELECT
-// As per PR 1938
-
-/* Homerow mods */
-
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left hand
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right hand
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumbs
-
-#define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
-  ZMK_BEHAVIOR(NAME, hold_tap, \
-      flavor = "balanced"; \
-      tapping-term-ms = <280>; \
-      quick-tap-ms = <QUICK_TAP_MS>; \
-      global-quick-tap-ms = <150>; \
-      bindings = <HOLD>, <TAP>; \
-      hold-trigger-key-positions = <TRIGGER_POS>; \
-      hold-trigger-on-release; \
-      )
-#define MAKE_HRM_SHIFT(NAME, HOLD, TAP, TRIGGER_POS) \
-  ZMK_BEHAVIOR(NAME, hold_tap, \
-      flavor = "balanced"; \
-      tapping-term-ms = <280>; \
-      quick-tap-ms = <QUICK_TAP_MS>; \
-      global-quick-tap-ms = <125>; \
-      bindings = <HOLD>, <TAP>; \
-      hold-trigger-key-positions = <TRIGGER_POS>; \
-      hold-trigger-on-release; \
-      )
-MAKE_HRM(hml, &kp, &kp, KEYS_R THUMBS)  // left-hand HRMs
-MAKE_HRM_SHIFT(hmls, &kp, &kp, KEYS_R THUMBS) // left-hand homerow mod for shift
-MAKE_HRM(hmr, &kp, &kp, KEYS_L THUMBS)  // right-hand
-MAKE_HRM_SHIFT(hmrs, &kp, &kp, KEYS_L THUMBS) // right-hand homerow mod for shift
-
-/* Nav cluster */
+&num_word {  // num-word, requires PR #1441
+    layers = <NUM>;
+};
 
 // long-tap action on hold, short-tap action on all interrupts
 #define MT_CORE \
@@ -67,16 +32,14 @@ MAKE_HRM_SHIFT(hmrs, &kp, &kp, KEYS_L THUMBS) // right-hand homerow mod for shif
     hold-trigger-key-positions = <0>;
 
 &mt { MT_CORE };
-ZMK_BEHAVIOR(mt_home, hold_tap, bindings = <&masked_home>, <&kp>; MT_CORE)
-ZMK_BEHAVIOR(mt_end,  hold_tap,  bindings = <&masked_end>, <&kp>; MT_CORE)
 
-#define NAV_LEFT  &mt_home 0   LEFT    // tap: left  | long-tap: beginning of line
-#define NAV_RIGHT &mt_end 0    RIGHT   // tap: right | long-tap: end       of line
-#define NAV_UP    &kp UP               // tap: up
-#define NAV_DOWN  &kp DOWN             // tap: down
-#define NAV_BSPC  &mt LA(BSPC) BSPC    // tap: bspc  | long-tap: delete word backward
-#define NAV_DEL   &mt LA(DEL)  DEL     // tap: del   | long-tap: delete word forward
+/* Apple Globe key */
+#define C_KEYBOARD_LAYOUT_SELECT (ZMK_HID_USAGE(HID_USAGE_CONSUMER, 0x029D))
+#define GLOBE C_KEYBOARD_LAYOUT_SELECT
+// As per PR 1938
 
+
+// Spectacle controls
 #define SPC_UP    &kp LA(LG(UP))
 #define SPC_DOWN  &kp LA(LG(DOWN))
 #define SPC_LEFT  &kp LA(LG(LEFT))
@@ -86,44 +49,78 @@ ZMK_BEHAVIOR(mt_end,  hold_tap,  bindings = <&masked_end>, <&kp>; MT_CORE)
 #define SPC_MVL   &kp LC(LA(LG(LEFT)))
 #define SPC_UNDO  &kp LC(LA(LG(Z)))
 
+// autopair macros
+ZMK_BEHAVIOR(mbkt, macro,
+    bindings = <&macro_tap &kp LBKT &kp RBKT &kp LEFT>;
+)
+ZMK_BEHAVIOR(mbrc, macro,
+    bindings = <&macro_tap &kp LBRC &kp RBRC &kp LEFT>;
+)
+ZMK_BEHAVIOR(mpar, macro,
+    bindings = <&macro_tap &kp LPAR &kp RPAR &kp LEFT>;
+)
+ZMK_BEHAVIOR(msqt, macro,
+    bindings = <&macro_tap &kp SQT &kp SQT &kp LEFT>;
+)
+ZMK_BEHAVIOR(mdqt, macro,
+    bindings = <&macro_tap &kp DQT &kp DQT &kp LEFT>;
+)
+// tap: '' | shift + tap: ""
+ZMK_BEHAVIOR(msqt_morph, mod_morph,
+    bindings = <&msqt>, <&mdqt>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+
+/* Homerow mods */
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left hand
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right hand
+#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                     // thumbs
+
+#define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
+  ZMK_BEHAVIOR(NAME, hold_tap, \
+      flavor = "balanced"; \
+      tapping-term-ms = <280>; \
+      quick-tap-ms = <QUICK_TAP_MS>; \
+      require-prior-idle-ms = <190>; \
+      bindings = <HOLD>, <TAP>; \
+      hold-trigger-key-positions = <TRIGGER_POS>; \
+      hold-trigger-on-release; \
+      )
+
+MAKE_HRM(hml, &kp, &kp, KEYS_R THUMBS)  // left-hand HRMs
+MAKE_HRM(hmr, &kp, &kp, KEYS_L THUMBS)  // right-hand
+
+#define MAKE_MACRO_HRM(NAME, HOLD, MACRO, TRIGGER_POS) \
+    ZMK_BEHAVIOR(NAME, hold_tap, \
+        flavor = "balanced"; \
+        tapping-term-ms = <280>; \
+        quick-tap-ms = <QUICK_TAP_MS>; \
+        require-prior-idle-ms = <190>; \
+        bindings = <HOLD>, <MACRO>; \
+        hold-trigger-key-positions = <TRIGGER_POS>; \
+        hold-trigger-on-release; \
+        )
 
 
 
 // SYM Layer home row mods
-#define SYM_AT     &hmr LCTRL AT
-#define SYM_LT     &hml LALT LT
-#define SYM_MINUS  &hml LGUI MINUS
+#define SYM_CARET  &hml LGUI CARET
+#define SYM_MINUS  &hml LALT MINUS
+#define SYM_PLUS   &hml LCTRL PLUS
 #define SYM_EQUAL  &hml LSHFT EQUAL
+MAKE_MACRO_HRM(mbkt_rshft, &kp RSHFT, &mbkt, KEYS_L THUMBS)
+#define SYM_MBKT   &mbkt_rshft
+#define SYM_RBKT   &hmr RCTRL RBKT
+#define SYM_AT     &hmr RALT AT
+#define SYM_DOLLAR &hmr RGUI DOLLAR
 
-#define SYM_LBKT   &hmr RSHFT LBKT
-#define SYM_RBKT   &hmr RGUI RBKT
-#define SYM_DOLLAR &hmr RALT DOLLAR
-#define SYM_FSLH   &hml RCTRL FSLH
 
-// NAV Layer Home row mods
-#define PREV_WINDOW &hml LCTRL LS(LG(GRAVE))
-#define NEXT_WINDOW &hml LALT LG(GRAVE)
-#define NAV_STAB    &hml LGUI LS(TAB)
-#define NAV_TAB     &hmls LSHFT TAB
-
-#define DSK_PREV    &kp LC(LEFT)      // previous desktop
-#define DSK_NEXT    &kp LC(RIGHT)      // next     desktop
-
-// TODO - figure out way to get swapper on index + middle finger with home row mods
-#define SWAP_PREV   &kp LS(TAB) // I'd like this to still work as a home row mod, but doesn't work with tri-state swapper
-// #define SWAPPER     &hml LSHFT &swapper // This doesn't work TODO - figure out how to bind swapper into a hold-tap
-
-// Quick Layer shortcuts
+// Mouse Layer command shortcuts
 // All keys become tap: command + (key) and long tap: shift + command + (key)
-#define Q_Q &mt LS(LG(Q)) LG(Q)
-#define Q_W &mt LS(LG(W)) LG(W)
 #define Q_F &mt LS(LG(F)) LG(F)
 #define Q_P &mt LS(LG(P)) LG(P)
 #define Q_B &mt LS(LG(B)) LG(B)
-#define Q_A &mt LS(LG(A)) LG(A)
-#define Q_R &mt LS(LG(R)) LG(R)
-#define Q_S &mt LS(LG(S)) LG(S)
-#define Q_T &mt LS(LG(T)) LG(T)
 #define Q_G &mt LS(LG(G)) LG(G)
 #define Q_Z &mt LS(LG(Z)) LG(Z)
 #define Q_X &mt LS(LG(X)) LG(X)
@@ -131,47 +128,26 @@ ZMK_BEHAVIOR(mt_end,  hold_tap,  bindings = <&masked_end>, <&kp>; MT_CORE)
 #define Q_D &mt LS(LG(D)) LG(D)
 #define Q_V &mt LS(LG(V)) LG(V)
 
-// Right hand hyper keys
-// need this to be able to preserve plain hyper modifier behavior on right hand while using left hyper key
-#define Q_J &mt LS(LG(J)) LG(J)
-#define Q_L &mt LS(LG(L)) LG(L)
-#define Q_U &mt LS(LG(U)) LG(U)
-#define Q_Y &mt LS(LG(Y)) LG(Y)
-#define Q_SQT &mt LS(LG(SQT)) LG(SQT)
-#define Q_M &mt LS(LG(M)) LG(M)
-#define Q_N &mt LS(LG(N)) LG(N)
-#define Q_E &mt LS(LG(E)) LG(E)
-#define Q_I &mt LS(LG(I)) LG(I)
-#define Q_O &mt LS(LG(O)) LG(O)
-#define Q_K &mt LS(LG(K)) LG(K)
-#define Q_H &mt LS(LG(H)) LG(H)
-#define Q_COMMA &mt LS(LG(COMMA)) LG(COMMA)
-#define Q_DOT &mt LS(LG(DOT)) LG(DOT)
-#define Q_QMARK &mt LS(LG(QMARK)) LG(QMARK)
-//
 // misc aliases
 #define LOCK_SCRN   &kp LC(LG(Q))
 #define NUM_MULTI   &kp KP_MULTIPLY
-#define CANCEL      &kp K_CANCEL             // cancel caps-word, num-word and smart-mouse
-#define LHYP      LS(LC(LA(LGUI)))
-#define RHYP      RS(RC(RA(RGUI)))
-
-
-// mask CTRL when holding left/right to avoid accidental jumps to beginning/end of document
-#define MASK_MODS(NAME, MODS, BINDING) \
-    ZMK_BEHAVIOR(NAME, mod_morph, \
-        bindings = <BINDING>, <BINDING>; \
-        mods = <MODS>; \
-    )
-MASK_MODS(masked_home, (MOD_LCTL), &kp HOME)
-MASK_MODS(masked_end,  (MOD_LCTL), &kp END)
-
+#define CANCEL      &kp K_CANCEL        // cancel caps-word, num-word and smart-mouse
+#define LHYP        LS(LC(LA(LGUI)))
+#define RHYP        RS(RC(RA(RGUI)))
+#define PASTE       &kp LG(V)
+#define UNDO        &kp LG(Z)
+#define REDO        &kp LS(LG(Z))
+#define PREV_WINDOW &kp LS(LG(GRAVE))
+#define NEXT_WINDOW &kp LG(GRAVE)
+/* #define DSK_PREV    &kp LC(LEFT)      // previous desktop */
+/* #define DSK_NEXT    &kp LC(RIGHT)      // next     desktop */
+#define SWAP_PREV   &kp LS(TAB)
 
 // tap: sticky-shift | shift + tap/ double-tap: caps-word | hold: shift
-ZMK_BEHAVIOR(smart_shft, mod_morph,
-    bindings = <&sk LSHFT>, <&caps_word>;
-    mods = <(MOD_LSFT)>;
-)
+//ZMK_BEHAVIOR(smart_shft, mod_morph,
+//    bindings = <&sk LSHFT>, <&caps_word>;
+//    mods = <(MOD_LSFT)>;
+//)
 
 
 // Command+Tab swapper, requires PR #1366
@@ -200,13 +176,6 @@ ZMK_BEHAVIOR(dot_inner_morph, mod_morph,
     mods = <(MOD_LCTL|MOD_RCTL)>;
 )
 
-// tap: qmark | shift + tap: excl
-ZMK_BEHAVIOR(qexcl, mod_morph,
-    bindings = <&kp QMARK>, <&kp EXCL>;
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-)
-
-
 // tap: space | shift + tap: underscore | hold: activate layer
 ZMK_BEHAVIOR(lt_spc, hold_tap,
     flavor = "balanced";
@@ -225,7 +194,6 @@ ZMK_BEHAVIOR(lt_del, hold_tap,
     tapping-term-ms = <200>;
     quick-tap-ms = <QUICK_TAP_MS>;
     bindings = <&mo>, <&bs_del>;
-
 )
 ZMK_BEHAVIOR(bs_del, mod_morph,
     bindings = <&kp BSPC>, <&kp DEL>;
@@ -245,9 +213,12 @@ ZMK_BEHAVIOR(sticky_num_dance, tap_dance,
     tapping-term-ms = <300>;
     bindings = <&sk LSHFT>, <&sl NUM>;  // reverse this for sticky-num on single tap
 )
-&num_word {  // num-word, requires PR #1441
-    layers = <NUM>;
-};
+ZMK_BEHAVIOR(copy_cut, hold_tap,
+    flavor = "balanced";
+    tapping-term-ms = <200>;
+    quick-tap-ms = <QUICK_TAP_MS>;
+    bindings = <&kp LG(X)>, <&kp LG(C)>;
+)
 
 
 // tap: caps word | double-tap: caps lock
@@ -255,4 +226,28 @@ ZMK_BEHAVIOR(caps_dance, tap_dance,
     tapping-term-ms = <200>;
     bindings = <&caps_word>, <&kp CAPS>;
 )
+ZMK_BEHAVIOR(comma_space, macro,
+    bindings = <&macro_tap &kp COMMA &kp SPACE>;
+)
+ZMK_BEHAVIOR(comma_dance, tap_dance,
+    tapping-term-ms = <200>;
+    bindings = <&kp SPACE>, <&comma_space>;
+)
+
+#define MAKE_LONG_HOLD(NAME, HOLD) \
+    ZMK_BEHAVIOR(NAME, hold_tap, \
+        flavor = "tap-preferred"; \
+        tapping-term-ms = <300>; \
+        quick-tap-ms = <QUICK_TAP_MS>; \
+        require-prior-idle-ms = <190>; \
+        bindings = <HOLD>, &none; \
+    )
+MAKE_LONG_HOLD(hbt, &bt)
+// TODO: confirm these actually work
+#define BT_0 &hbt BT_SEL 0
+#define BT_1 &hbt BT_SEL 1
+#define BT_2 &hbt BT_SEL 2
+#define BT_3 &hbt BT_SEL 3
+#define BT_4 &hbt BT_SEL 4
+
 
