0e90857ca7763998516577cffb983bac661a4c46
teinzl <154447128+teinzl@users.noreply.github.com>
2024-10-09 22:36:08 +0900

    Update custom_behaviors.h

--- config/custom_behaviors.h
+++ config/custom_behaviors.h
@@ -137,19 +137,7 @@ ZMK_BEHAVIOR(msqt, macro,
 ZMK_BEHAVIOR(mdqt, macro,
     bindings = <&macro_tap &kp DQT &kp DQT &kp LEFT>;
 )
-// tap: '' | shift + tap: ""
-ZMK_BEHAVIOR(msqt_morph, mod_morph,
-    bindings = <&msqt>, <&mdqt>;
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-)
-// tab navigation macros
-ZMK_BEHAVIOR(mtabnext, macro,
-    bindings = <&macro_tap &kp G &kp T>;
-)
-ZMK_BEHAVIOR(mtabprev, macro,
-    bindings =
-        <&macro_tap &kp G &kp LS(T)>;
-)
+
 
 // tap: sticky-shift | shift + tap/ double-tap: caps-word | hold: shift
 //ZMK_BEHAVIOR(smart_shft, mod_morph,
@@ -159,88 +147,6 @@ ZMK_BEHAVIOR(mtabprev, macro,
 
 
 // Command+Tab swapper, requires PR #1366
-ZMK_BEHAVIOR(swapper, tri_state,
-    bindings = <&kt LGUI>, <&kp TAB>, <&kt LGUI>;
-    ignored-key-positions = <LB2>, <20>; // ignored key should be position of SWAP_PREV
-)
-
-// tap: comma | shift + tap: semicolon | ctrl + shift + tap: <
-ZMK_BEHAVIOR(comma_morph, mod_morph,
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-    bindings = <&kp COMMA>, <&comma_inner_morph>;
-)
-ZMK_BEHAVIOR(comma_inner_morph, mod_morph,
-    bindings = <&kp SEMICOLON>, <&kp LESS_THAN>;
-    mods = <(MOD_LCTL|MOD_RCTL)>;
-)
-
-// tap: dot | shift + tap: colon | ctrl + shift + tap: >
-ZMK_BEHAVIOR(dot_morph, mod_morph,
-    bindings = <&kp DOT>, <&dot_inner_morph>;
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-)
-ZMK_BEHAVIOR(dot_inner_morph, mod_morph,
-    bindings = <&kp COLON>, <&kp GREATER_THAN>;
-    mods = <(MOD_LCTL|MOD_RCTL)>;
-)
-
-// tap: qmark | shift + tap: excl
-ZMK_BEHAVIOR(qexcl, mod_morph,
-    bindings = <&kp QMARK>, <&kp EXCL>;
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-)
-
-
-// tap: space | shift + tap: underscore | hold: activate layer
-ZMK_BEHAVIOR(lt_spc, hold_tap,
-    flavor = "balanced";
-    tapping-term-ms = <200>;
-    quick-tap-ms = <QUICK_TAP_MS>;
-    bindings = <&mo>, <&spund>;
-)
-ZMK_BEHAVIOR(spund, mod_morph,
-  bindings = <&kp SPACE>, <&kp UNDERSCORE>;
-  mods = <(MOD_LSFT|MOD_RSFT)>;
-)
-
-// tap: backspace | lshft + tap: delete | rshft + tap: shift-delete | hold: activate layer
-ZMK_BEHAVIOR(lt_del, hold_tap,
-    flavor = "balanced";
-    tapping-term-ms = <200>;
-    quick-tap-ms = <QUICK_TAP_MS>;
-    bindings = <&mo>, <&bs_del>;
-)
-ZMK_BEHAVIOR(bs_del, mod_morph,
-    bindings = <&kp BSPC>, <&kp DEL>;
-    mods = <(MOD_LSFT|MOD_RSFT)>;
-    keep-mods = <MOD_RSFT>;
-)
-
-// tap: sticky shift | double-tap: sticky num-layer | hold: num-layer
-ZMK_BEHAVIOR(shift_num, hold_tap,
-    flavor = "balanced";
-    tapping-term-ms = <200>;
-    quick-tap-ms = <QUICK_TAP_MS>;
-    bindings = <&mo>, <&sticky_num_dance>;
-)
-ZMK_BEHAVIOR(sticky_num_dance, tap_dance,
-    tapping-term-ms = <300>;
-    bindings = <&sk LSHFT>, <&sl NUM>;  // reverse this for sticky-num on single tap
-)
-
-// tap: caps word | double-tap: caps lock
-ZMK_BEHAVIOR(caps_dance, tap_dance,
-    tapping-term-ms = <200>;
-    bindings = <&caps_word>, <&kp CAPS>;
-)
-ZMK_BEHAVIOR(comma_space, macro,
-    bindings = <&macro_tap &kp COMMA &kp SPACE>;
-)
-// tap: space | double-tap: comma + space | shift + tap: underscore
-ZMK_BEHAVIOR(comma_dance, tap_dance,
-    tapping-term-ms = <200>;
-    bindings = <&spund>, <&comma_space>;
-)
 
 
 
